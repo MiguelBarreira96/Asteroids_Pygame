169aa37ec8a7dfa940ba6166df911263717b687e
MiguelBarreira96 <miguelbarreira96@gmail.com>
2025-06-05 21:44:04 +0100

    add: fire rate

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y ):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -24,6 +25,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt 
 
         if keys[pygame.K_a]:
             self.rotate(dt)
@@ -42,6 +44,10 @@ class Player(CircleShape):
 
     def shoot(self):
         velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        #print(self.timer)
+        if self.timer > 0:
+            return
         shot = Shot(self.position.x , self.position.y, velocity)
+        self.timer += PLAYER_SHOOT_COOLDOWN
         
         
